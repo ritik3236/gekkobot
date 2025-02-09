1dd095ee2c4a60f79bd84585de52edf0d268ba1c
Ritik <Ritikkushwaha1234@gmail.com>
2025-02-10 03:54:55 +0530

    Get XBot Balance

--- src/lib/configs.ts
+++ src/lib/configs.ts
@@ -5,7 +5,7 @@ export const botsConfigs: BotConfig[] = [
         adminChatId: +process.env.TELEGRAM_ADMIN_CHAT_ID,
         botName: 'PayoutX',
         chatIds: [-4644489581, 1282110140],
-        polling: true,
+        polling: false,
         token: process.env.TELEGRAM_BOT_TOKEN_XBOT,
         webhookUrl: 'https://gekkobot-delta.vercel.app/api/telegram/xbot',
     },
